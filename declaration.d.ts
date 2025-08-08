d342f42e14a2bc4fe2777dc0bd7c1a6d945f397e
AlexCK7 <to03491@georgiasouthern.edu>
2025-08-08 10:36:48 -0500

    Final pre-review commit for UrbanDrive: frontend/backend synced, gitignore cleaned, files updated

--- declaration.d.ts
+++ declaration.d.ts
@@ -1,1 +1,12 @@
-declare module '@react-native-picker/picker';
+// declaration.d.ts
+declare module "*.svg" {
+  import * as React from "react";
+  const content: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default content;
+}
+
+declare module "@react-native-picker/picker";
+
+declare global {
+  type UserRole = "user" | "driver" | "admin";
+}
